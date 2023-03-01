2175a61cde8a9965ceeed2351f1728914e138e0e
a20caros <a20caros@student.his.se>
2023-03-01 11:52:40 +0100

    Added Browserrouter
    Added BrowserRouter around routers to get a historical context that´s needed for the data to execute on the pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import {Routes, Route } from 'react-router-dom';
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
 import Home from './Elements/Home';
 
 const App=()=>{
   return (
     <>
+    <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Home/>}/>
     </Routes>
+    </BrowserRouter>
     </>
   )
 }
