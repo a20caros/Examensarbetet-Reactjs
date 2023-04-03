a68873f56666c12a6dfc2acd4ee0c3253d562139
a20caros <a20caros@student.his.se>
2023-04-03 11:31:08 +0200

    Took new photos that are creative common
    Took new photos that are creative common

--- src/Elements/imagesInfo.js
+++ src/Elements/imagesInfo.js
@@ -1,17 +1,3 @@
-import flowerPerfume from './pexels-darina-belonogova-8789609.jpg';
-import bluePerfume from './pexels-daria-liudnaya-8166566.jpg';
-import sandPerfume from './pexels-karolina-grabowska-8361478.jpg';
-import greenPerfume from './pexels-karolina-grabowska-8361487.jpg';
-import orangePerfume from './pexels-daria-liudnaya-8166972.jpg';
-import silkesPerfume from './pexels-hanna-auramenka-8553210.jpg';
-import orangeFlowerPerfume from './pexels-dids-1190829.jpg';
-import purpleFlowerPerfume from './pexels-daria-liudnaya-8166613.jpg';
-import orangeMenPerfume from './pexels-darina-belonogova-8789599.jpg';
-import darkOrangePerfume from './pexels-rfstudio-3059609.jpg';
-import greenCapPerfume from './pexels-dina-nasyrova-3831748.jpg';
-import goldPerfume from './pexels-syed-nouman-7400855.jpg';
-import darkBluePerfume from './pexels-jonathan-cooper-15190739.jpg';
-import darkGoldPerfume from './pexels-yogesh-jangid-7702669.jpg';
 import parfymeImg from './pexels-cottonbro-studio-4659794.jpg';
 import essentialOilImg from './pexels-cottonbro-studio-4659792.jpg';
 import parfymeHand from './pexels-artem-podrez-6801188.jpg';
@@ -29,74 +15,54 @@ const ImagesInfo=()=>{
                 <th>Länk till bild</th>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={flowerPerfume}/></td>
-                <td><p>Darina Belonogova</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/stenar-flaska-spray-gatustenar-8789609/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gfe5dbd7feb18fcb2656392d837f37c452d5cde1cd53f60a921175a67b547dfd8e4e0751915f23474581357eca3e3db168c844e41443e7bed507a1972a0ae0c8f162736f06e3d2b8ebce79d282c753933_1920.jpg"}/></td>
+                <td><p>Sponchia</p></td>
+                <td><a href="https://pixabay.com/sv/photos/s%c3%a4ker-parfym-doft-flaska-flacon-3635196/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation'src={bluePerfume}/></td>
-                <td><p>Daria Liudnaya</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/prototyp-flaska-stilleben-parfym-8166570/">Länk</a></td>
+                <td><img className='imagesInformation'src={"https://pixabay.com/get/g2548eecc3c781bf820293e192b37c9171fdffb304281c2199e7397d9fe91dcc5eccab302cb254fc6c9d5c68a735867fa160521e9a36eb1a1ff712d220354ec02ed99c5af89d8f1c33566f4bf05a53584_1920.jpg"}/></td>
+                <td><p>monicore</p></td>
+                <td><a href="https://pixabay.com/sv/photos/flaska-parfym-parfymflaska-glas-2387210/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation'src={sandPerfume}/></td>
-                <td><p>Karolina Grabowska</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/strand-sand-vatten-kust-8361478/">Länk</a></td>
+                <td><img className='imagesInformation'src={"https://pixabay.com/get/g84130591908d150f80c8a463eeb570c2f8ade78348ec9d765ade33d9bc82b5652448ac227027a7742df19fcf1dabb93fbe9216ebbe9ba362a79e336ecb28ebb465deb5c4b91d60079207a932f1397ac0_1920.jpg"}/></td>
+                <td><p>shahidawan</p></td>
+                <td><a href="https://pixabay.com/sv/photos/skrivbord-parfym-glas-utrustning-3022322/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={greenPerfume}/>
-                </td><td><p>Karolina Grabowska</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/sand-prototyp-flaska-doft-8361479/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gfcbf1a420b212763115ba31f5537a42ba2bdb87b29b5e682d356dd1453194db5d6699b0f29ebf99caed4c48717bea057301d79246604a8a516ef2cea9f0cfc70d007aa3050a4ea22e969518ddac5ecb1_1920.jpg"}/></td>
+                <td><p>Passagere</p></td>
+                <td><a href="https://pixabay.com/sv/photos/parfym-kork-blomma-flaska-glas-1495651/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={orangePerfume}/></td>
-                <td><p>Daria Liudnaya</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/prototyp-plattform-flaska-behallare-8166972/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g7e84b5604f49dd1a8f3b741371cc4a4caa6715d1dba73ecc1e9c761874bc6a959a0a924012517cdf0f421f6ea6eeeec2cf32f97eed7230b826f4506ed07c4becce43fce119b2e3b86237a320d882c932_1920.jpg"}/></td>
+                <td><p>Positive_Images</p></td>
+                <td><a href="https://pixabay.com/sv/photos/parfym-flaska-fortfarande-liv-guld-4232929/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={silkesPerfume}/></td>
-                <td><p>Hanna Auramenka</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/mode-design-flaska-parfym-8553210/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gb4f72878bf7a9a96e1af12ced76a0bab8dc16af93a0c8dd97f6fd13bd7a0d774f2411567ed1941a20df63460bd74ae8b7dace2489e3b1c0f87c5d649ce8ac1437c44378a3c6b94782ff03f541bbd9d7b_1920.jpg"}/></td>
+                <td><p>yogeshjangid1997may</p></td>
+                <td><a href="https://pixabay.com/sv/photos/parfym-doft-flaska-sk%c3%b6nhet-6223754/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={orangeFlowerPerfume}/>
-                </td><td><p>Dids</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/glas-lyx-flaska-doft-1190829/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g541ae67308ace6a4db2e4c40f5a7f0772a1b62b702948ebd871008b6e2395fa8087f943e151060d89a2c23044e0af840bf9346ba9278fe7d042833099abadd49614be6da13708c40ab465e4f9f1fb9dd_1920.jpg"}/></td>
+                <td><p>Mohammad_Rasheed</p></td>
+                <td><a href="https://pixabay.com/sv/photos/attar-parfym-olja-m%c3%b6rk-arv-2126745/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={purpleFlowerPerfume}/></td>
-                <td><p>Daria Liudnaya</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/glas-flaska-parfym-narbild-8166613/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g0e4b083a63950d5d6da8e91b282122fafc860cfb05d98c31cd242b1a8fa1ffc9c8d8037b5d7811f8de5cc0bb624e483e77154856ab415e381d529f1232648ecbf8c81e8718577d1b6b95f118056d6d8e_1920.jpg"}/></td>
+                <td><p>Randgruppe</p></td>
+                <td><a href="https://pixabay.com/sv/photos/parfym-flaska-ljus-4252281/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={orangeMenPerfume}/></td>
-                <td><p>Darina Belonogova</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/flaskor-parfym-forpackning-behallare-8789599/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gcf0255b5df59e74e5fb3f5522241f9465ad75d47568d49d4fc2310907ffb65b7188653f8c930e943662dab18f422fff440efecfba6236f47f7cc6867d3be5e876d4fcbde043e2e591e5f67065127d12a_1920.jpg"}/></td>
+                <td><p>steinchen</p></td>
+                <td><a href="https://pixabay.com/sv/photos/bl%c3%a5-flaska-flaska-bl%c3%a5-parfym-242361/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={darkOrangePerfume}/></td>
-                <td><p>RF._.studio</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/skuggor-rok-aromatisk-spray-3059609">Länk</a></td>
-            </tr>
-            <tr>
-                <td><img className='imagesInformation' src={greenCapPerfume}/></td>
-                <td><p>Dina Nasyrova</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/glas-flaska-doft-vatska-3831748/">Länk</a></td>
-            </tr>
-            <tr>
-                <td><img className='imagesInformation' src={goldPerfume}/></td>
-                <td><p>Syed Nouman</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/doft-parfym-dyr-produktfotografering-7400855/">Länk</a></td>
-            </tr>
-            <tr>
-                <td><img className='imagesInformation' src={darkBluePerfume}/></td>
-                <td><p>Jonathan Cooper</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/doft-parfym-produktfotografering-mont-blanc-15190739/">Länk</a></td>
-            </tr>
-            <tr>
-                <td><img className='imagesInformation' src={darkGoldPerfume}/></td>
-                <td><p>YOGESH JANGID</p></td>
-                <td><a href="https://www.pexels.com/sv-se/foto/prototyp-guld-flaska-spray-7702669/">Länk</a></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/ge6afbd318cb420569682f5562092b03c40429e8438d0d4d5d9cf618a51a3e5714ba649fa619911d99907769c7a83aba2591c4c09213fd860a2c6ac78ef9b0e7c9f6237a72826b5be67d190275e440c0f_1920.jpg"}/></td>
+                <td><p>Josch13</p></td>
+                <td><a href="https://pixabay.com/sv/photos/parfym-glasflaska-parfymflaska-227100/">Länk</a></td>
             </tr>
             <tr>
                 <td><img className='imagesInformation' src={parfymeImg}/></td>
