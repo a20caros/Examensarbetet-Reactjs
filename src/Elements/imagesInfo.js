0e7e95d235f77b0a8807ded7521526e278e3dbbd
a20caros <a20caros@student.his.se>
2023-04-04 15:38:09 +0200

    Changed the picture links
    Changed the picture links

--- src/Elements/imagesInfo.js
+++ src/Elements/imagesInfo.js
@@ -15,57 +15,57 @@ const ImagesInfo=()=>{
                 <th>Länk till bild</th>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/gfe5dbd7feb18fcb2656392d837f37c452d5cde1cd53f60a921175a67b547dfd8e4e0751915f23474581357eca3e3db168c844e41443e7bed507a1972a0ae0c8f162736f06e3d2b8ebce79d282c753933_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gb671507566e56f85b5177517b1b173dbb2631f749361c22e8aed915b4733c84dc21eba6baf6bd5ec3d64b76c88bb3a64a36315f4d3f2199c7916b39d550351569aed54b9572b0757cfb67132debfa365_1920.jpg"}/></td>
                 <td><p>Sponchia</p></td>
                 <td><a href="https://pixabay.com/sv/photos/s%c3%a4ker-parfym-doft-flaska-flacon-3635196/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation'src={"https://pixabay.com/get/g2548eecc3c781bf820293e192b37c9171fdffb304281c2199e7397d9fe91dcc5eccab302cb254fc6c9d5c68a735867fa160521e9a36eb1a1ff712d220354ec02ed99c5af89d8f1c33566f4bf05a53584_1920.jpg"}/></td>
+                <td><img className='imagesInformation'src={"https://pixabay.com/get/g2f94c92b9a6565b14d7a4beb2e035982360661f5a70f928a7eb8807a04a216682e1f588d4939bad7c26f1c4abe17b7355b40a884dad3dbc8cfadb21f606ef9e2af90073bc4e5ee538128d097da730c3d_1920.jpg"}/></td>
                 <td><p>monicore</p></td>
                 <td><a href="https://pixabay.com/sv/photos/flaska-parfym-parfymflaska-glas-2387210/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation'src={"https://pixabay.com/get/g84130591908d150f80c8a463eeb570c2f8ade78348ec9d765ade33d9bc82b5652448ac227027a7742df19fcf1dabb93fbe9216ebbe9ba362a79e336ecb28ebb465deb5c4b91d60079207a932f1397ac0_1920.jpg"}/></td>
+                <td><img className='imagesInformation'src={"https://pixabay.com/get/gfc1268586deafe72dac85a0c9207610ac43e89ccf391758e9fe1371a824de2c445dfc2b2838e31ce57eba6e4f06b1f1cb8ada49c6aa0b8444ca445010d425d6ebc380f0ce33afd1bd826346a006bd1c2_1920.jpg"}/></td>
                 <td><p>shahidawan</p></td>
                 <td><a href="https://pixabay.com/sv/photos/skrivbord-parfym-glas-utrustning-3022322/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/gfcbf1a420b212763115ba31f5537a42ba2bdb87b29b5e682d356dd1453194db5d6699b0f29ebf99caed4c48717bea057301d79246604a8a516ef2cea9f0cfc70d007aa3050a4ea22e969518ddac5ecb1_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gc33082d76239d020b247220487a17723ccd4e154813cd3cdc223a40265fbd26de522ffc74103adc340ff84f33a2d9376eace57f5d382fdd5a7f685760e4c8f96b8253b8be15fb457d9a1d10f0eb26aa3_1920.jpg"}/></td>
                 <td><p>Passagere</p></td>
                 <td><a href="https://pixabay.com/sv/photos/parfym-kork-blomma-flaska-glas-1495651/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/g7e84b5604f49dd1a8f3b741371cc4a4caa6715d1dba73ecc1e9c761874bc6a959a0a924012517cdf0f421f6ea6eeeec2cf32f97eed7230b826f4506ed07c4becce43fce119b2e3b86237a320d882c932_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g5ae9e0835eb96251482d57a3e55e4430e7c56c3d176896158b35b490bf9dc0335a77f423952187c8a5a3324ee02f4f56041751a63ee084ff90733fd5b64290bfd1c8955ff083d97e8133925a815edead_1920.jpg"}/></td>
                 <td><p>Positive_Images</p></td>
                 <td><a href="https://pixabay.com/sv/photos/parfym-flaska-fortfarande-liv-guld-4232929/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/gb4f72878bf7a9a96e1af12ced76a0bab8dc16af93a0c8dd97f6fd13bd7a0d774f2411567ed1941a20df63460bd74ae8b7dace2489e3b1c0f87c5d649ce8ac1437c44378a3c6b94782ff03f541bbd9d7b_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/gb387a5aebd29469f6affc997bb56246f1835d8218c991b0e09670eb05627a887fa24c289b75eba330a22c1c94d57304aca00da9d8a4058eed25735b96d9e75cb7fa53e64a19f7320f93a9c1e3d424b60_1920.jpg"}/></td>
                 <td><p>yogeshjangid1997may</p></td>
                 <td><a href="https://pixabay.com/sv/photos/parfym-doft-flaska-sk%c3%b6nhet-6223754/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/g541ae67308ace6a4db2e4c40f5a7f0772a1b62b702948ebd871008b6e2395fa8087f943e151060d89a2c23044e0af840bf9346ba9278fe7d042833099abadd49614be6da13708c40ab465e4f9f1fb9dd_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g71cdabb9df79d70b1ceacd450cf964658644395136a45b6b4975c49d845cc28567439225762239b511b2df978ff57483cae29243f8ccbc3d05ec09bbe9e23d0b4059757f937b6452bc9d4c84799492ea_1920.jpg"}/></td>
                 <td><p>Mohammad_Rasheed</p></td>
                 <td><a href="https://pixabay.com/sv/photos/attar-parfym-olja-m%c3%b6rk-arv-2126745/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/g0e4b083a63950d5d6da8e91b282122fafc860cfb05d98c31cd242b1a8fa1ffc9c8d8037b5d7811f8de5cc0bb624e483e77154856ab415e381d529f1232648ecbf8c81e8718577d1b6b95f118056d6d8e_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/ga82bf78ddbe9be72b42f60c138a0d1ec83f214a0aabe21d3277225d18215eefa8699fd6569bf6efb56a3f93da7c87b7deb33f09cc080f758f76b8da247dbfbdf120936643578d0822a4cb36db91efb23_1920.jpg"}/></td>
                 <td><p>Randgruppe</p></td>
                 <td><a href="https://pixabay.com/sv/photos/parfym-flaska-ljus-4252281/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/gcf0255b5df59e74e5fb3f5522241f9465ad75d47568d49d4fc2310907ffb65b7188653f8c930e943662dab18f422fff440efecfba6236f47f7cc6867d3be5e876d4fcbde043e2e591e5f67065127d12a_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g99306a967ad3a8230bb7750062542784b1d1f4e7113c743c8b3a3baea27cb324a0871bc074f8dcf31791d34f83acd7a364045d9b46fb3bbfd8e2d8aaeb196b25b8e4d9a42115fa137cee76b0877d9a73_1920.jpg"}/></td>
                 <td><p>steinchen</p></td>
                 <td><a href="https://pixabay.com/sv/photos/bl%c3%a5-flaska-flaska-bl%c3%a5-parfym-242361/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/ge6afbd318cb420569682f5562092b03c40429e8438d0d4d5d9cf618a51a3e5714ba649fa619911d99907769c7a83aba2591c4c09213fd860a2c6ac78ef9b0e7c9f6237a72826b5be67d190275e440c0f_1920.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g07c70afb8975a41ab77a2f34cb854e28d8c7f410a4c2d103b01e9ab2b7865c276012fcd1fcbbb6a1d43d6ca7e8879e166cd7d6aac238aec08d16c9949a784790c6ec53728bef7612e754bf4a8aa42759_1920.jpg"}/></td>
                 <td><p>Josch13</p></td>
                 <td><a href="https://pixabay.com/sv/photos/parfym-glasflaska-parfymflaska-227100/">Länk</a></td>
             </tr>
             <tr>
-                <td><img className='imagesInformation' src={"https://pixabay.com/get/g77783ce50a103f762774e8995d1de46a80046223745e3f3164098c4acee7b18e651b1d92983fcb00b2d49407d190739efe5b37ab2476e9bb57065b9df24281e04e43beb363d1c5a24fa9fd634da961d9_1280.jpg"}/></td>
+                <td><img className='imagesInformation' src={"https://pixabay.com/get/g3d84250a1fa6730586ec209cb5827a0c1c76012af3a68d0f23be9dec31c045d6035e2daf2ff77051eff3b1b6d44a246867e9c1ccc1b030461eaffb4c0866df6a965d96c3093109865b2941bf8e4fb9fe_1280.jpg"}/></td>
                 <td><p>Victoria_Watercolor</p></td>
                 <td><a href="https://pixabay.com/photos/hipsters-girls-coquette-perfume-4213520/">Länk</a></td>
             </tr>
